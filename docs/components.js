bdce5db8a9e7ba62d3365a57c50d2e0009a1ea7b
CarlosBF <cborrellf@outlook.es>
2025-05-27 00:46:23 +0200

    Documentation implemented following class directions

---
+++ docs/components.js
@@ -0,0 +1,26 @@
+module.exports = {
+    components:{
+        schemas:{
+            Task:{
+                type:'object',
+                properties:{
+                    _id:{
+                        type:'objectId',
+                        description:"task identification number",
+                        example:"6201064b0028de7866e2b2c4"
+                    },
+                    title:{
+                        type:'string',
+                        description:"Name of the task",
+                        example:"Make documentation"
+                    },
+                    completed:{
+                        type:'boolean',
+                        description:"Whether task is completed or not",
+                        example:true
+                    },
+                }
+            }
+        }
+    }
+}
